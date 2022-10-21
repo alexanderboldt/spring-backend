d5e8febac15b796214244bf312469f0cead9189f
Alexander Boldt <alexx1216@live.de>
2022-10-21 11:48:46 +0200

    added postgres-dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,14 +17,16 @@ repositories {
 }
 
 dependencies {
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("org.postgresql:postgresql:42.5.0")
 }
 
 tasks.withType<KotlinCompile> {

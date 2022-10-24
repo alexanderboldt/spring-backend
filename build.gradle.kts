3d853acfdbbd0a2e38a7f527c38fa48b708c3ed7
Alexander Boldt <alexx1216@live.de>
2022-10-24 11:32:03 +0200

    cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,13 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
 	implementation("org.postgresql:postgresql:42.5.0")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
